3e2025d15730dcb2dba929b386e1ae6f24e757c9
manojonkar <manojonkar@gmail.com>
2026-09-23 11:24:54 +0530

    Initial scaffold: web app, .gitignore, shared structure

---
+++ web/pages/_app.tsx
@@ -0,0 +1,19 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
